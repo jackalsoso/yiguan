6522af6ba9470bfc164252d00d96b8475c2b2b4d
yiming_shou@ssic.cn <yiming_shou@ssic.cn>
2020-08-18 12:01:01 +0800

    更新

---
+++ src/pages/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'moreList',
+  data(){
+    return{
+
+    }
+  },
+  created(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .login{
+
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,64 @@
 import Vue from 'vue'
+// import bus from '@/bus'
 import Router from 'vue-router'
-import routes from './modules/routes'
+// import Cookie from '@/utils/cookie.js';
+import store from '@/vuex';
 
 Vue.use(Router)
 
+// 登录
+const Login = r => require(['@/pages/login'], r)
+
+// 常量路由
+export const constantRoutes = [
+  {
+    path: '/login',
+    name:'Login',
+    component: Login,
+    meta: {
+      auth: true// true为不进行权限验证,false或不写默认进行验证
+    }
+  },
+]
+// 动态路由
 const router = new Router({
-	linkActiveClass:'active',
-  routes: routes
+  scrollBehavior:()=>({y:0}),
+  routes:constantRoutes
 })
 
-//授权登陆
-// router.beforeEach((to, from, next) => {
-
-// })
-
-router.afterEach(() => {
-  window.scrollTo(0,0);
+Vue.mixin({
+  beforeRouteEnter(to, from, next){
+    next(vm => {
+      bus.$on(`${to.path}pageRefresh`,() => {
+        vm.refresh && vm.refresh()
+      })
+    })
+  },
+  beforeRouteLeave(to, from, next){
+    bus.$off(`${from.path}pageRefresh`)
+    next();
+  }
 })
 
-
-export default router
+// 路由监听
+router.beforeEach((to, from, next) =>{
+  // const token = Cookie.getCookie('token'), userInfo = sessionStorage.getItem(userInfo)
+  // if(token){
+  //   if(!store.state.user.systemDict){
+  //     // store.state.user.getSystemDict({})
+  //     store.dispatch('getSystemDict', {})
+  //   }
+  //   if(to.path === "/login"){
+  //     next('/');
+  //     return;
+  //   }
+  //   next()
+  // }else{
+  //   if(to.path !== "/login"){
+  //     next('/login');
+  //     return;
+  //   }
+  //   next()
+  // }
+})
+export default router;
